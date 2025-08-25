2958cd618957f4fdcfde64de0548720c25f3eba1
Carol Vilchez <design.harls@icloud.com>
2025-08-25 12:53:09 +0200

    Funcionalidad Dibujar y borrar
    Drawing app funcionando permite dibujar , escoger un color para el pincel y borrar el lienzo.

--- js/canvas.js
+++ js/canvas.js
@@ -1,65 +1,71 @@
 // Se obtiene el elemento <canvas> del DOM utilizando getElementById, y se obtiene su contexto de representación bidimensional ('2d') mediante getContext. El contexto es necesario para realizar operaciones de dibujo en el lienzo.
 const canvas = document.getElementById('drawingCanvas');
 const ctx = canvas.getContext('2d');
-const tool = document.getElementById('container-tools')
+const toolbar = document.getElementById('toolbar')
 
 
 
-// Variables para rastrear el estado del mouse
-// Se declaran variables para rastrear el estado del mouse (isDrawing indica si se está dibujando), y las coordenadas lastX y lastY para almacenar la posición del mouse en el último evento.
-let isDrawing = false;
-let lastX = 0;
-let lastY = 0;
+const canvasOffsetX = canvas.offsetLeft;
+const canvasOffsetY = canvas.offsetTop;
 
 
+canvas.width = window.innerWidth - canvasOffsetX;
+canvas.height = window.innerHeight - canvasOffsetY;
 
 
-// Se agregan cuato eventos del ratón
-canvas.addEventListener('mousedown', startDrawing);//Inicio del dibujo
-canvas.addEventListener('mousemove', draw);//dibujo continuo
-canvas.addEventListener('mouseup', stopDrawing);//fin del dibujo
-canvas.addEventListener('mouseout', stopDrawing);//cuando el ratón sale del area del canva, tambien se considera como fin del dibujo
+let isPainting = false;
+let lineWidth = 5;
+let startX;
+let startY;
 
 
+toolbar.addEventListener('click', e => {
+    if (e.target.id === 'clear') {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+});
 
-// Función para comenzar a dibujar
-function startDrawing(e) {
-  isDrawing = true;
-  [lastX, lastY] = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
-}
-//startDrawing se llama cuando se presiona el botón del mouse. Establece isDrawing en true y actualiza las coordenadas lastX y lastY con la posición actual del mouse relativa al canvas.
+// cambiar el color 
+toolbar.addEventListener('change', e => {
+    if (e.target.id === 'stroke') {
+        ctx.strokeStyle = e.target.value;
+    }
+
+    if (e.target.id === 'lineWidth') {
+        lineWidth = e.target.value;
+    }
+});
 
 
+// funcion de dibujar
+const draw = (e) => {
+    if (!isPainting) {
+        return;
+    }
 
-// Función para dibujar
-function draw(e) {
-  if (!isDrawing) return;
-  const [x, y] = [e.clientX - canvas.offsetLeft, e.clientY - canvas.offsetTop];
-
-  // Configurar el estilo del trazo
-  ctx.lineWidth = 5;
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = '#000';
-
-  // Comenzar el trazo
-  ctx.beginPath();
-  ctx.moveTo(lastX, lastY);
-
-  // Dibujar una línea hasta la posición actual
-  ctx.lineTo(x, y);
-
-  // Aplicar el trazo
-  ctx.stroke();
-
-  // Actualizar la última posición
-  // draw se llama mientras el mouse se mueve. Si isDrawing es true, obtiene las coordenadas actuales del mouse, configura el estilo del trazo, comienza un nuevo trazo (beginPath), mueve el punto de inicio a la posición anterior (moveTo), dibuja una línea hasta la posición actual (lineTo), y aplica el trazo (stroke). Luego, actualiza las coordenadas lastX y lastY.
-  [lastX, lastY] = [x, y];
+    ctx.lineTo(e.clientX - canvasOffsetX, e.clientY);
+    ctx.lineWidth = lineWidth;
+    ctx.lineCap = 'round';
+    ctx.stroke();
 }
 
 
+// controles de dibujo 
 
-// Función para dejar de dibujar
-//stopDrawing se llama cuando el mouse se levanta o sale del área del canvas. Establece isDrawing en false, indicando que se ha completado el dibujo.
-function stopDrawing() {
-  isDrawing = false;
-}
+canvas.addEventListener('mousedown', (e) => {
+    isPainting = true;
+    startX = e.clientX;
+    startY = e.clientY;
+});
+
+
+canvas.addEventListener('mouseup', e => {
+    isPainting = false;
+    ctx.stroke();
+    ctx.beginPath();
+});
+
+canvas.addEventListener('mousemove', draw);
+
+
+// comprobamos si estamos dibujando o no
